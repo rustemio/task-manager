34981d462394dd1cb65d7c8686b237d1604aa2c1
Rustem's MacBook Pro <rustemio@RUSTEMs-MacBook-Pro-2.local>
2025-05-10 23:37:12 +0300

    GET /tasks endpoint added

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, supports_credentials=True, origins=["http://localhost:3000"])
+CORS(app)
 
 tasks = []
 
@@ -14,7 +14,8 @@ def get_tasks():
 def add_task():
     data = request.get_json()
     tasks.append(data)
-    return jsonify({"message": "Görev eklendi!"}), 201
+    return jsonify({"message": "Task is added!"}), 201
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5001, debug=True)
+@app.route('/')
+def index():
+    return "Flask backend is working!"
