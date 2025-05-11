a26bbeb2f4996810680e52a35fa1162e6a8ea251
Rustem's MacBook Pro <rustemio@RUSTEMs-MacBook-Pro-2.local>
2025-05-11 09:53:53 +0300

    Move app.py to root so Azure can see /tasks

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify, request # type: ignore
+from flask_cors import CORS # type: ignore
+
+app = Flask(__name__)
+CORS(app)
+
+tasks = []
+
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify(tasks)
+
+@app.route('/tasks', methods=['POST'])
+def add_task():
+    data = request.get_json()
+    tasks.append(data)
+    return jsonify({"message": "Task is added!"}), 201
+
+@app.route('/')
+def index():
+    return "Flask backend is working!"
